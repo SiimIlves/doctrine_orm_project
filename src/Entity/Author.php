aab1fc14c0bd7b2492e08776af17ef0e7351e817
SiimIlves <65825538+SiimIlves@users.noreply.github.com>
2021-03-14 20:55:17 +0200

    test tabeli testandmed

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -2,58 +2,51 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collection\ArrayCollection;
 
 /**
- * @Entity
- */
-class Author
-{
-    /**
-     * @Column(type="integer")
-     * @Id
-     * @GeneratedValue
-     */
-    private $id;
+* @Entity
+*/
+class Author{
+	/**
+	* @Column(type="integer")
+	* @Id
+	* @GeneratedValue
+	*/
+	private $id;
 
-    /**
-     * @Column(type="string")
-     */
-    private $name;
+	/**
+	* @Column(type="string")
+	*/
+	private $name;
 
-    /**
-     * @OneToMany(targetEntity="Article", mappedBy="author")
-     * @OrderBy({"publiched" = "DESC"})
-     */
-    private $articles;
+	/**
+	* @OneToMany(targetEntity="Article", mappedBy="author")
+	* @OrderBy({"publiched" = "DESC"})
+	*/
+	private $articles;
 
-    public function __construct()
-    {
-        $this->articles = new ArrayCollection;
-    }
+	public function __construct(){
+		$this->articles = new ArrayCollection;
+	}
 
-    public function getId()
-    {
-        return $this->id;
-    }
+	public function getId(){
+		return $this->id;
+	}
 
-    public function setId($value)
-    {
-        $this->id = $value;
-    }
+	public function setId($value){
+		$this->id = $value;
+	}
 
-    public function getName()
-    {
-        return $this->name;
-    }
+	public function getName(){
+		return $this->name;
+	}
 
-    public function setName($value)
-    {
-        $this->name = $value;
-    }
+	public function setName($value){
+		$this->name = $value;
+	}
 
-    public function getArticles()
-    {
-        return $this->articles;
-    }
+	public function getArticles(){
+		return $this->articles;
+	}
 }
